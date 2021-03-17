26550a891079f30f69edd1c70c4958f65af35e7d
Erdem Demircioglu <64333517+erdem6161@users.noreply.github.com>
2021-03-17 23:11:05 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http'
+import{FormsModule} from "@angular/forms"
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductComponent } from './components/product/product.component';
+import { CategoryComponent } from './components/category/category.component';
+import { NaviComponent } from './components/navi/navi.component';
+import { TodoComponent } from './components/todo/todo.component';
+import { VatAddedPipe } from './pipes/vat-added.pipe';
+import { from } from 'rxjs';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductComponent,
+    CategoryComponent,
+    NaviComponent,
+    TodoComponent,
+    VatAddedPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
